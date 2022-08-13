0d89c6ad3f2ae1c617e39354b9dd195e5538af44
felipe <flipeoliveira007@gmail.com>
2022-08-13 16:36:36 -0300

    att

--- CadastroAlunoVB/frmCadastroAluno.vb
+++ CadastroAlunoVB/frmCadastroAluno.vb
@@ -1,5 +1,49 @@
 ﻿Public Class frmCadastroAluno
+    Dim strConexao As String = "Data Source = (local)\sqlexpress;UID=nutec;PWD=nutec;initial catalog=NUTEC;"
+
+    Private Sub btnIncluir_Click(sender As Object, e As EventArgs) Handles btnIncluir.Click
+        Dim conexao As New System.Data.SqlClient.SqlConnection
+        conexao.ConnectionString = strConexao
+        conexao.Open()
+
+        Dim comando As New System.Data.SqlClient.SqlCommand
+        comando.Connection = conexao
+        comando.CommandText = ""
+        comando.ExecuteNonQuery()
+
+        conexao.Close()
+
+
+    End Sub
+
+
+
+
+
+
     Private Sub frmCadastroAluno_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub GroupBox3_Enter(sender As Object, e As EventArgs) Handles GroupBox3.Enter
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub txtcodigo_TextChanged(sender As Object, e As EventArgs) Handles txtcodigo.TextChanged
+        'teste"
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click, Label6.Click
+
+    End Sub
+
+
 End Class
